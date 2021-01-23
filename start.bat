1455c9e6af7af8f4b116645e2946ca90e5257681
aym <alexum2013@yandex.ru>
2021-01-23 14:57:07 +0300

    сборка

--- start.bat
+++ start.bat
@@ -1,1 +1,4 @@
-docker run --volume "%cd%":/app web-engine:develop
+docker build -t web-engine:develop .
+docker stop webber
+docker rm webber
+docker run -p 5000:5000 --name webber --volume "%cd%":/app web-engine:develop
